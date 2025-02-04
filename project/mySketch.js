a8b490d76ac71a158ebe7b1216ee773b32461fdf
hugo984 <26hugo.loeb@lwhs.org>
2025-02-04 20:04:08 +0000

    random

--- project/mySketch.js
+++ project/mySketch.js
@@ -3,9 +3,10 @@ let yLocation = 75
 let xLocation = 205
 let xLocation2 = 195
 let yLocation2 = 75
+let shapes = [circles, squares, triangles];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(405, 400);
   background(50);
   noStroke();
   fill(255);
@@ -14,10 +15,12 @@ function setup() {
 function draw() { 
 }
 
+
 function keyPressed(){
   if(keyCode === LEFT_ARROW){
     fill("pink")
-    ellipse(xLocation2,yLocation2,10,10)
+    let data2 = shapes[int(random(0, shapes.length))];
+    data2(xLocation2,yLocation2)
 
     if(xLocation2 === 5){
       yLocation2 = yLocation2+10
@@ -28,12 +31,23 @@ function keyPressed(){
 
   if(keyCode === RIGHT_ARROW){
     fill("green")
-    ellipse(xLocation,yLocation,10,10)
+    let data = shapes[int(random(0, shapes.length))];
+    data(xLocation,yLocation)
   if (xLocation === 395) {
     yLocation = yLocation+10 
     xLocation = 195
   }
     xLocation = xLocation + 10
   }  
-   
+}
+
+
+function circles (xLocation, yLocation){
+ ellipse(xLocation + 5,yLocation + 5, 10, 10)
+}
+function squares (xLocation, yLocation){
+  square(xLocation, yLocation, 9.5)
+}
+function triangles (xLocation,yLocation){
+  triangle(xLocation + 5, yLocation, xLocation + 10, yLocation + 5 * sqrt(3), xLocation, yLocation + 5 * sqrt(3))
 }
